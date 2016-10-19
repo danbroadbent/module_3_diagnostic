ff3285989e345674991c76910d7d5b80fd74e317
Daniel Broadbent <dbroadbent85@gmail.com>
2016-10-19 10:16:43 -0600

    Added Faraday connection and search view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 class SearchController < ApplicationController
   def index
-    
+    conn = Faraday.new(:url => 'https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?') do |faraday|
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+    response = conn.get '/nrel/alt-fuel-stations/v1/nearest.json?', { :api_key => ENV["nrel_api_key"], :location => params[:q], :limit => 10 }
+    @results = JSON.parse(response.body)
   end
 end
